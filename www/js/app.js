fd326aa0fa9a454b6f13fc3ae25d66e194a6c37c
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2014-11-10 19:03:21 +0100

    Jakinarazpenen js-a gehitu eta lehen probak egin

--- www/js/app.js
+++ www/js/app.js
@@ -4,27 +4,65 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('ikuslang-app', [
+var app = angular.module('ikuslang-app', [
     'ionic',
     'ngDragDrop',
     'ui.sortable',
     'ikuslang-app.controllers',
     'ikuslang-app.services',
-    'ikuslang-app.directives'
+    'ikuslang-app.directives',
+    'cordova'
 ])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+.run(function($ionicPlatform, push) {
+    
+    $ionicPlatform.ready(function() {
+        
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleDefault();
+        }
+        
+        var result = push.registerPush(function (result) {
+            console.log(result);
+            
+            if (result.type === 'registration') {
+                
+                console.log("erregistratuta");
+                
+            } else if (result.type === 'message') {
+                
+                console.log("mezua jasota");
+                console.log(result);
+                
+                // Mezua jasotzean aplikazioa 3 egoeratan egon daiteke:
+                // * Aurreko planoan exekutatzen -> foreground === true
+                // * Atzeko planoan ezkutuan baina memorian -> foreground === false && coldstart === false
+                // * Erabat geldi -> foreground === false && coldstart === true
+                if (result.foreground) {
+                    
+                } else {
+                    
+                    if (result.coldstart) {
+                        
+                    } else {
+                        
+                    }
+                    
+                }
+                
+                // Alertaren elementura bideratu.
+                document.location.href = "#/tab/nabarmenduak-xehetasunak/" + result.id_elementua;
+            }
+        });
+        
+    });
+    
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
